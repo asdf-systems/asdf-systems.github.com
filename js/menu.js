c548618b2c08e04eb75d75aa3808bd5ccee20537
Alexander Surma <alexander.surma@gmail.com>
2011-11-02 00:07:05 +0100

    I'm a moron. Using Javascript for links instead of <a>

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-	// Add click handler for menu items
-	// - Remove active class from all
-	// - add active to the clicked one
-	$('nav ul li').click(function(elem) {
-		window.location.href = "/"+$.trim($(this).text()).toLowerCase()+".html";
-	});
-})(jQuery);
