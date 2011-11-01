905796bfae4f25652f0809af66f2c894a263d2ec
Alexander Surma <alexander.surma@gmail.com>
2011-11-01 21:26:48 +0100

    Fixed color changing for IE8

--- js/corners.js
+++ js/corners.js
@@ -1,17 +1,15 @@
 (function ($) {
+	var colorClasses = ['turquoise', 'pink', 'blue'];
 	var getColorClass = function(obj) {
-		for(var i = 0; i < obj.classList.length; i++) {
-			switch(obj.classList[i]) {
-				case "turquoise":
-				case "pink":
-				case "blue":
-					return obj.classList[i];
+		for(index in colorClasses) {
+			if($(obj).hasClass(colorClasses[index])) {
+				return colorClasses[index];
 			}
 		}
 		return "";
 	}
 	// Change corners on portfolio page
-	var $corners = $("#corner_top, #corner_bottom");
+	var $corners = $('#corner_top, #corner_bottom');
 	$('#portfolio dt').click(function() {
 		$corners.removeClass(getColorClass($corners[0])).addClass(getColorClass(this));
 	})
