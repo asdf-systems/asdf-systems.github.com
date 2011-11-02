67a3ac40d4741ef55e051cf91012648a1fc2f67d
Alexander Surma <alexander.surma@gmail.com>
2011-11-02 12:32:27 +0100

    Open first accordion section by default

--- js/accordion.js
+++ js/accordion.js
@@ -1,16 +1,21 @@
 (function($) {
 	// Handle accordion clicks
+	// - open first section by defaults
+	//   - except if it got the class "closed"
 	// - on header click:
 	//   - remove active class from all
 	//   - add active class to the clicked one
 	$('dl.accordion').each(function(elem) {
 		var $accordion = $(this);
-		var headers = $accordion.children('dt');
-		$(headers).click(function() {
+		var $headers = $($accordion.children('dt'));
+		if(!$accordion.hasClass('closed') && $headers.length > 0) {
+			$($headers[0]).addClass('active');
+		}
+		$headers.click(function() {
 			if($(this).hasClass('active')) {
 				$(this).removeClass('active');
 			} else {
-				$(headers).removeClass('active');
+				$headers.removeClass('active');
 				$(this).addClass('active');
 			}
 		})
