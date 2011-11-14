9d05052dd24ee4393a930d1f35e6effed79dd669
Alexander Surma <alexander.surma@gmail.com>
2011-11-14 15:38:28 +0100

    Added keywords and added support for imageless posts

--- js/accordion.js
+++ js/accordion.js
@@ -2,6 +2,7 @@
 	// Handle accordion clicks
 	// - open first section by defaults
 	//   - except if it got the class "closed"
+	//   - or if the fragment adresses on of the headers
 	// - on header click:
 	//   - remove active class from all
 	//   - add active class to the clicked one
@@ -11,12 +12,17 @@
 
 		var openTargets = function() {
 			var targets = location.hash.split('#');
+			var found = false;
 			$headers.each(function() {
 				if($.inArray($(this).attr('id'), targets) != -1) {
 					$(this).addClass('active');
+					found = true;
 					return;
 				}
 			})
+			if(!found) {
+				$($headers[0]).addClass('active');
+			}
 		}
 		openTargets();
 
